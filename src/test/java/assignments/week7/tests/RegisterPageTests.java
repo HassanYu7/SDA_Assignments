00ab0d4ee60f19e8ed247f213fa04955aaa75a41
Hassan Alhejaili <65440093+HassanYu7@users.noreply.github.com>
2024-03-27 07:15:45 +0300

    Refactor Task1 classes for week7

--- src/test/java/assignments/week7/tests/RegisterPageTests.java
+++ src/test/java/assignments/week7/tests/RegisterPageTests.java
@@ -4,25 +4,27 @@ import assignments.TestBase;
 import org.testng.annotations.Test;
 import pages.RegisterPage;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 public class RegisterPageTests extends TestBase {
 
-         /*
-            go to url : https://ecommerce-playground.lambdatest.io/index.php?route=account/register
-            register account test with fluent page object approach
-         */
 
-    @Test
-    public void test() {
-        new RegisterPage(driver, bot)
-                .navigateToRegisterPage()
-                .register("ahmed",
-                        "mohamed",
-                        "dsads@hotmail.com",
-                        "09666e34",
-                        "2323",
-                        true
-                );
+    @Test(description = "Verify successful registration with valid information")
+    public void verifySuccessfulRegistration() {
+        assertEquals("Your Account Has Been Created!",
+                new RegisterPage(driver, bot)
+                        .navigateTo()
+                        .successfulRegister("Hassan", "Alharbi", "test9@gmail.com", "4334", "545445", "545445")
+                        .getSuccessfulMessageText());
+    }
 
+    @Test(description = "Registration failure due to invalid password length")
+    public void verifyFailedRegistrationInvalidPasswordLength() {
+        assertEquals("Password must be between 4 and 20 characters!",
+                new RegisterPage(driver, bot)
+                        .navigateTo()
+                        .failedRegister("Anas", "Ahmed", "test10@gmail.com", "34433", "33", "33")
+                        .failureMessage());
     }
 
 
