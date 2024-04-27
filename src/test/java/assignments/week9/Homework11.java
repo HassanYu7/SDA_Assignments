7c2a35fa1d462073fee955e300cc538a3c7dc1a9
Hassan Alhejaili <65440093+HassanYu7@users.noreply.github.com>
2024-04-27 20:05:34 +0300

    Updated Task11 for week9

--- src/test/java/assignments/week9/Homework11.java
+++ src/test/java/assignments/week9/Homework11.java
@@ -1,7 +1,5 @@
 package assignments.week9;
-import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
